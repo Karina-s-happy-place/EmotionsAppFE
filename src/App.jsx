51de04c36d2e85efc6a60ce8f36aa7228be4a0f8
karinalorenzo <karinalrg16@gmail.com>
2025-04-20 03:10:33 +0200

    clean branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,6 @@
 import "./styles/styles.css";
-import soft from "../src/assets/img/soft.jpg";
 
-function App() {
-  return (
-    <div
-      className="bg-[#F1E5DA] min-h-svh font-averia"
-      style={{
-        backgroundImage: `url(${soft})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
-      <h1 className="text-[#392F24] text-[2rem]">Welcome to React</h1>
+export default function App() {
 
-      <p className="text-[#392F24]">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa,
-        fermentum id id vitae, integer fermentum tellus. In vitae id nisl quis
-        ornare diam commodo in vel dolor..
-      </p>
-    </div>
-  );
 }
 
-export default App;
